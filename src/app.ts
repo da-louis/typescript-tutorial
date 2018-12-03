ea8928cdaf948c37d4696c6d2ee6d4b990c0cbc4
da_louis <ruaces@gmail.com>
2018-12-03 22:03:34 +0900

    change price test

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,5 @@ import { Item } from "./item";
 
 const elem = document.getElementById("output");
 
-const aBook = new Item("はじめてのTypeScript", 1980);
+const aBook = new Item("はじめてのTypeScript", 3980);
 aBook.say(elem);
